71ab4a3f75fe358d562a58f06bf376c70355338e
mahe-f <fmaahekaan@gmail.com>
2026-01-04 19:20:57 +0500

    Add files via upload

--- A1.py
+++ A1.py
@@ -3,6 +3,8 @@ pygame.init()
 screen=pygame.display.set_mode((500,500))
 pygame.display.set_caption("Game")
 bg=pygame.transform.scale(pygame.image.load("bg.jpg"),(500,500))
+text=pygame.font.Font(None,36).render("Welcome to my game",True,pygame.Color("White"))
+text_react=text.get_rect(center=(500//2,500//2))
 done=False
 while not done:
     for event in pygame.event.get():
@@ -10,4 +12,5 @@ while not done:
             pygame.quit()
             
     screen.blit(bg,(0,0))
+    screen.blit(text,text_react)
     pygame.display.flip()
